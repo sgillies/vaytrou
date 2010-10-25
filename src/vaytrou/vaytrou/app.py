7fd6f51d2c32413c0ee40e69d62e302a1cd60981
sgillies <sean.gillies@gmail.com>
2010-10-24 23:11:38 -0600

    Removed "strict" search, added configuration of various query parameter ranges.

--- src/vaytrou/vaytrou/app.py
+++ src/vaytrou/vaytrou/app.py
@@ -16,6 +16,11 @@ from vrtree import VIntRtreeIndex
 
 define('data', default=None, help='Data storage directory')
 define('port', default=8888, help='Server port')
+define('page_size', type=int, default=20, help='Search result page size')
+define('max_page_size', type=int, default=100, help='Maximum search result page size')
+define('max_nearest_limit', type=int, default=20, help='Maximum limit for nearest neighbor search')
+define('max_bbox_area', type=float, default=None, help='Maximum search box area')
+define('max_radius', type=float, default=200000.0, help='Maximum radius for distance search')
 
 def appmaker(root):
     if not 'index' in root:
@@ -43,6 +48,23 @@ def geom(item):
         b = item['bbox']
         g = box2poly(b)
     return asShape(g)
+
+class Error(Exception):
+    pass
+
+class SearchBoxTooLarge(Error):
+    def __init__(self, bbox, value):
+        self.bbox = bbox
+        self.value = value
+    def __str__(self):
+        return "Area of %s exceeds %s" % (self.bbox, self.value)
+
+class SearchRadiusTooLarge(Error):
+    def __init__(self, radius, value):
+        self.radius = radius
+        self.value = value
+    def __str__(self):
+        return "Radius %s exceeds %s" % (self.radius, self.value)
 
 class Application(tornado.web.Application):
     def __init__(self, index=None, handlers=None, default_host="",
@@ -78,17 +100,28 @@ class IntersectionHandler(tornado.web.RequestHandler):
         try:
             bbox = self.get_argument('bbox')
             coords = tuple(float(x) for x in bbox.split(','))
+            if len(coords) == 4 and options.max_bbox_area is not None:
+                if geom({'bbox': coords}).area > options.max_bbox_area:
+                    raise SearchBoxTooLarge(bbox, options.max_bbox_area)
             # Paging args
             start = int(self.get_argument('start', '0'))
-            count = int(self.get_argument('count', '20'))
-            if count > 20: count = 20
+            count = int(self.get_argument('count', '0')) or options.page_size
+            if count > options.max_page_size:
+                count = options.max_page_size
             # Query
             candidates = self.application.index.intersection(coords)
             hits = list(candidates)
             results = hits[start:start+count]
             self.set_status(200)
             self.set_header('content-type', 'application/json')
-            self.write(dumps(dict(bbox=coords, start=start, count=len(results), hits=len(hits), items=results)))
+            self.write(dumps(
+                dict(
+                    bbox=coords, 
+                    start=start, 
+                    count=len(results), 
+                    hits=len(hits), 
+                    items=results)
+                ))
         except:
             raise
 
@@ -98,12 +131,22 @@ class NearestHandler(tornado.web.RequestHandler):
         try:
             bbox = self.get_argument('bbox')
             coords = tuple(float(x) for x in bbox.split(','))
+            if len(coords) == 4 and options.max_bbox_area is not None:
+                if geom({'bbox': coords}).area > options.max_bbox_area:
+                    raise SearchBoxTooLarge(bbox, options.max_bbox_area)
             limit = int(self.get_argument('limit', '1'))
-            if limit > 20: limit = 20
+            if limit > options.max_nearest_limit:
+                limit = options.max_nearest_limit
             results = list(self.application.index.nearest(coords, limit))
             self.set_status(200)
             self.set_header('content-type', 'application/json')
-            self.write(dumps(dict(bbox=bbox, limit=limit, count=len(results), items=results)))
+            self.write(dumps(
+                dict(
+                    bbox=bbox, 
+                    limit=limit, 
+                    count=len(results), 
+                    items=results)
+                ))
         except:
             raise
 
@@ -114,9 +157,13 @@ class DistanceHandler(tornado.web.RequestHandler):
             x = float(self.get_argument('lon'))
             y = float(self.get_argument('lat'))
             r = float(self.get_argument('radius'))
+            if options.max_radius is not None:
+                if r > options.max_radius:
+                    raise SearchRadiusTooLarge(r, options.max_radius)
             start = int(self.get_argument('start', '0'))
-            count = int(self.get_argument('count', '20'))
-            if count > 20: count = 20
+            count = int(self.get_argument('count', '0')) or options.page_size
+            if count > options.max_page_size: 
+                count = options.max_page_size
             # Make a box for first pass intersection search
             Re = 6371000.0 # radius of earth in meters, spherical approx
             F = 180.0/(math.pi*Re)
@@ -132,7 +179,15 @@ class DistanceHandler(tornado.web.RequestHandler):
             results = hits[start:start+count]
             self.set_status(200)
             self.set_header('content-type', 'application/json')
-            self.write(dumps(dict(lon=x, lat=y, radius=r, start=start, count=len(results), hits=len(hits), items=results)))
+            self.write(dumps(
+                dict(
+                    lon=x, 
+                    lat=y, 
+                    radius=r, 
+                    start=start, 
+                    count=len(results), 
+                    hits=len(hits), items=results)
+                ))
         except:
             raise
 
